ba6cfb6191f869d3eb8ee7204aa52507b4c89c8c
chenqi99 <chenqi99@nancal.com>
2024-08-29 17:17:09 +0800

    init

--- src/tests/test.py
+++ src/tests/test.py
@@ -1,4 +1,6 @@
-import torch
+
+
+
 # from torch import __version__; from packaging.version import Version as V
 # xformers = "xformers==0.0.27" if V(__version__) < V("2.4.0") else "xformers"
 #
@@ -10,8 +12,15 @@ import torch
 # print(cmd)
 
 
-print("是否可用：", torch.cuda.is_available())        # 查看GPU是否可用
-print("GPU数量：", torch.cuda.device_count())        # 查看GPU数量
-print("torch方法查看CUDA版本：", torch.version.cuda)  # torch方法查看CUDA版本
-print("GPU索引号：", torch.cuda.current_device())    # 查看GPU索引号
-print("GPU名称：", torch.cuda.get_device_name(1))    # 根据索引号得到GPU名称
+# print("是否可用：", torch.cuda.is_available())        # 查看GPU是否可用
+# print("GPU数量：", torch.cuda.device_count())        # 查看GPU数量
+# print("torch方法查看CUDA版本：", torch.version.cuda)  # torch方法查看CUDA版本
+# print("GPU索引号：", torch.cuda.current_device())    # 查看GPU索引号
+# print("GPU名称：", torch.cuda.get_device_name(1))    # 根据索引号得到GPU名称
+
+from datasets import load_dataset
+
+dataset = load_dataset("yahma/alpaca-cleaned", split = "train")
+# dataset = load_dataset("data/files/alpaca_data_cleaned", split = "train")
+
+print(dataset)
